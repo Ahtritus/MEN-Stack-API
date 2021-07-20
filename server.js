c784d65f92f75d60e7b2ca5b46c08f779967e600
Ahtritus <sutirthabharatidas@gmail.com>
2021-07-21 00:43:03 +0530

    author routes

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author");
 
 app.set("view engine", "ejs");
 app.set("layout", "layouts/layout");
 app.set("views", __dirname + "/views");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
+app.use(express.json());
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,5 +27,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to MongoDB!"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
